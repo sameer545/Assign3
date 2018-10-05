37ae06da60e9b3b6fb186585250da1da7c2dc2f6
sameer545 <abdull.sameerr@gmail.com>
2018-10-05 17:27:26 +0530

    Add project files.

---
+++ Assign3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+
+            Console.WriteLine("odd numbers are");
+            while(i<50)
+            {
+                
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(+i);
+                    }
+                i++;  
+            }
+        }
+    }
+}
